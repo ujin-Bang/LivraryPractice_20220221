ff81ed3eb2c43920efed2eff054e5ece0a0149ce
ujin_bang <qkddnwls14@gmail.com>
2022-02-21 11:52:28 +0900

    사진 크게 보기 화면 추가  - 라이브러리 활용은 아님

--- app/src/main/java/com/example/livrarypractice_20220221/MainActivity.kt
+++ app/src/main/java/com/example/livrarypractice_20220221/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.livrarypractice_20220221
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupEvents()
+        setValues()
+    }
+
+    fun setupEvents(){
+
+//        버튼 이외 TextView, ImageView, LinearLayout 등등도 setOnClickListener로 이벤트 처리 가능함.
+        imgProfile.setOnClickListener {
+
+//            사진을 크게 보는 액티비티로 이동
+            val myIntent = Intent(this, ViewPhotoActivity::class.java)
+            startActivity(myIntent)
+
+        }
+    }
+    fun setValues(){
+
     }
 }
